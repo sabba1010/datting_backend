b9d58483281dd1b3178a235e710025f3ff45f5b1
Sabba Hossain <sabbahossain@gmail.com>
2026-03-15 21:13:48 +0600

    add

--- Server.js
+++ Server.js
@@ -25,7 +25,17 @@ mongoose.connect(process.env.MONGODB_URI)
     .catch(err => console.error('MongoDB Connection Error:', err));
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:8080',
+        'http://localhost:5000',
+        'https://amour-et-sincerite.com'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
